bade7de3a8e5b07ca61ba59e9453f79a1c5e845d
sugiyan0519 <b1019261@fun.ac.jp>
2021-12-03 15:17:25 +0900

    add : get_data, get_acc, get_gyro, get_mag, is_auto_step

--- CanSat/Vmodule.py
+++ CanSat/Vmodule.py
@@ -3,16 +3,18 @@ import math
 
 class Vmodule:
 
-    def __init__(self, path):
+    def __init__(self, path, is_auto_step):
         self.config_path = './' + path + '.csv'
         self.process = 0
         self.data_set = [0,0,0,0,0,0]
+        self.goal = [41.833705, 140.770666]
+        self.is_auto_step = is_auto_step
 
         #csv to list
         with open(self.config_path, 'r') as csv_file:
             csv_reader = reader(csv_file)
             self.process_list = list(csv_reader)
-
+            
         self.len = len(self.process_list)
             
     def step(self, n):
@@ -23,6 +25,26 @@ class Vmodule:
             self.process += int(n)
         else:
             self.process = 0
+
+    def get_data(self):
+        if self.is_auto_step:
+            self.step(1)
+        return self.data_set
+
+    def get_acc(self):
+        if self.is_auto_step:
+            self.step(1)
+        return self.data_set
+
+    def get_gyro(self):
+        if self.is_auto_step:
+            self.step(1)
+        return self.data_set
+
+    def get_mag(self):
+        if self.is_auto_step:
+            self.step(1)
+        return self.data_set
         
     def getLatitude(self):
         return self.data_set[0]
